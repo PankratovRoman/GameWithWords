8f4bf84537e30b867fdd0369401da14b693d22a4
PankratovRoman <r.u.pankratov@gmail.com>
2021-04-12 17:07:12 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace GameWithWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите набор букв: ");
+            string shifr = Console.ReadLine();
+            while (true)
+               ReadLine(shifr);
+
+        }
+
+        static bool ReadLine(string shifr)
+        {
+            Console.WriteLine("Введите искомое слово: ");
+            string word = Console.ReadLine();
+            bool[] allBool = new bool[word.Length]; // сюда будем класть тру или фолсы, которые получатся при поиске букв в шифр слове
+            
+            for (var i = 0; i < word.Length; i++ )
+            {
+                if (shifr.Contains(word[i]))
+                {
+                    allBool[i] = true;
+                }
+            }
+            foreach(var elem in allBool)
+            {
+                if (elem == false)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
